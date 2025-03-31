ed04d5d1c02217afa3434535bc319fc403c6431d
ODGARZONC <odgarzc@gmail.com>
2025-03-30 19:54:21 -0500

    cambio para mostrar ruta principal en Render

--- core/urls.py
+++ core/urls.py
@@ -6,10 +6,12 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from .views import NotificacionesView
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Ruta para la página de inicio
+    path('', views.index, name='index'),  # Ruta principal
+    #path('', views.home, name='home'),  # Ruta para la página de inicio
     path('api/aprobaciones/', AprobacionListCreate.as_view(), name='aprobaciones'),
     path('protected/', ProtectedView.as_view(), name='protected_view'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/notificaciones/', NotificacionesView.as_view(), name='notificaciones')
+
 ]
 
